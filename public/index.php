a7f1971ac3951eb275b0b848e67076354392284f
Orley Galán <orleigalan@gmail.com>
2025-12-15 13:52:43 -0500

    Eliminar var_dump y exit del index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php
 
-// 1️⃣ Cargar variables de entorno
+//Cargar variables de entorno
 require_once __DIR__ . '/../bootstrap.php';
 
-// 2️⃣ Cargar la clase de conexión
+// Cargar la clase de conexión
 require_once __DIR__ . '/../config/conexion_db.php';
 
-// 3️⃣ Usar la conexión
+// Usar la conexión
 $db = new Conexion_db();
 $conn = $db->conectar();
 
-var_dump($_ENV['MYSQLHOST']);
-exit;
+?>
