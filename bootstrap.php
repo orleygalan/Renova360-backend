8fc748fa4b4c38bfd7cc2f3afc58dcc396649753
Orley Galán <orleigalan@gmail.com>
2025-12-15 15:28:35 -0500

    solucion

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+if (file_exists(__DIR__.'/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
