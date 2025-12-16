234225d7f950ab3047d1e5b74e4c4f549b16e6b6
Orley Galán <orleigalan@gmail.com>
2025-12-15 23:05:22 -0500

    comprobando conexion con la db prueba9

--- public/index.php
+++ public/index.php
@@ -8,11 +8,11 @@ try {
     $conn = $db->conectar();
 
     $stmt = $conn->query("SELECT DATABASE() AS db");
-    $data = $stmt->fetch();
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo json_encode([
         "success" => true,
-        "categorias" => [$data]
+        "data" => $data
     ]);
 } catch (PDOException $e) {
     http_response_code(500);
@@ -21,3 +21,26 @@ try {
         "error" => $e->getMessage()
     ]);
 }
+
+// require '../cros.php';
+// require_once __DIR__ . '/../bootstrap.php';
+// require_once __DIR__ . '/../config/conexion_db.php';
+
+// try {
+//     $db = new Conexion_db();
+//     $conn = $db->conectar();
+
+//     $stmt = $conn->query("SELECT DATABASE() AS db");
+//     $data = $stmt->fetch();
+
+//     echo json_encode([
+//         "success" => true,
+//         "data" => [$data]
+//     ]);
+// } catch (PDOException $e) {
+//     http_response_code(500);
+//     echo json_encode([
+//         "success" => false,
+//         "error" => $e->getMessage()
+//     ]);
+// }
