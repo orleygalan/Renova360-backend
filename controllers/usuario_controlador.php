da02f4c3933b285dbf3fa6fcb5630045ff084610
Orley Galán <orleigalan@gmail.com>
2025-12-21 19:00:54 -0500

    Envios de correro electronicos4

--- controllers/usuario_controlador.php
+++ controllers/usuario_controlador.php
@@ -65,7 +65,7 @@ class Usuario_controlador
             $mail->SMTPAuth = true;
             $mail->Username = $_ENV['SMTP_USER'];
             $mail->Password = $_ENV['SMTP_PASS'];
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
             $mail->Port = $_ENV['SMTP_PORT'];
 
             $mail->setFrom($_ENV['SMTP_FROM'], 'Renova360');
